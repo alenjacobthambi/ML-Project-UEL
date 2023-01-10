78d2fc13991e08b5a9d534864e6d80925d8cbfe7
alenjacobthambi <alenjacobthambi@gmail.com>
2023-01-10 14:24:19 +0000

    Added sidebar

--- ml_project_main.py
+++ ml_project_main.py
@@ -1,1 +1,7 @@
 import streamlit as st
+
+st.sidebar.title('Welcome to ML Project')
+page = st.sidebar.radio(
+    "What would you like to know about this project ?",
+    ("Data Exploration", 'Machine Learning Models')
+)
