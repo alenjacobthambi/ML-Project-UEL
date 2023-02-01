388488767be6ed6d091aef3fa2a36e8cb162da3f
alenjacobthambi <alenjacobthambi@gmail.com>
2023-02-01 23:50:35 +0000

    matplotlib removed

--- ml_project_main.py
+++ ml_project_main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Lasso
