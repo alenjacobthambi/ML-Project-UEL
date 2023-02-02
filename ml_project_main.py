ca929aaaae904a218ec6bd438e2b299e01a35ace
alenjacobthambi <alenjacobthambi@gmail.com>
2023-02-02 00:51:10 +0000

    Removed method

--- ml_project_main.py
+++ ml_project_main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.tree import DecisionTreeRegressor
